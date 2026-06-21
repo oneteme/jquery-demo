f8315ed0b2d34e0255c6da81380580513619e086
u$f <usf.alami@gmail.com>
2026-06-22 01:18:42 +0200

    edit

--- src/main/java/io/github/oneteme/jquery/demo/config/CommonRequestQueryResolver.java
+++ src/main/java/io/github/oneteme/jquery/demo/config/CommonRequestQueryResolver.java
@@ -7,7 +7,6 @@ import static java.util.stream.Stream.concat;
 import static org.usf.jquery.core.Utils.isEmpty;
 import static org.usf.jquery.web.Parameters.COLUMN_PARAM;
 import static org.usf.jquery.web.Parameters.CRITERIA_OPR;
-import static org.usf.jquery.web.Parameters.CTE_PARAM;
 import static org.usf.jquery.web.Parameters.FILTER_OPR;
 import static org.usf.jquery.web.Parameters.SELECT_PARAM;
 import static org.usf.jquery.web.Parameters.VIEW_PARAM;
@@ -17,13 +16,15 @@ import static org.usf.jquery.web.proxy.StoreManager.getInstance;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Supplier;
 
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
-import org.usf.jquery.core.QueryComposer;
+import org.usf.jquery.web.Parameters;
+import org.usf.jquery.web.proxy.MvcRequest;
 import org.usf.jquery.web.proxy.QueryInterpreter;
 import org.usf.jquery.web.proxy.QueryRequest;
 import org.usf.jquery.web.proxy.Restriction;
@@ -33,44 +34,84 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class CommonRequestQueryResolver implements HandlerMethodArgumentResolver, QueryInterpreter {
-
+	
+	private static final String[] DEFAULT_VIEWER = new String[]{"json"};
+	
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
-        return QueryComposer.class.isAssignableFrom(parameter.getNestedParameterType())
-                && parameter.hasParameterAnnotation(QueryRequest.class);
+    	if(parameter.hasMethodAnnotation(QueryRequest.class)) {
+            return parameter.getNestedParameterType() == MvcRequest.class 
+//            		|| DatasetResource.class.isAssignableFrom(parameter.getNestedParameterType())
+//            		|| ResultSetMapper.class.isAssignableFrom(parameter.getNestedParameterType())
+            		;
+    	}
+    	return false;
     }
-
-    @Override
+    
+	@Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
                                   NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        var ann = parameter.getParameterAnnotation(QueryRequest.class);
-        if(isNull(ann)) {
-            throw new IllegalStateException("missing @QueryRequest annotation");
+    	 var ann = parameter.getMethodAnnotation(QueryRequest.class);
+         if(isNull(ann)) {
+             throw new IllegalStateException("missing @QueryRequest annotation");
+ 		}
+    	if(parameter.getNestedParameterType() == MvcRequest.class) {
+			return resolveQueryComposer(ann, parameter, webRequest);
 		}
+    	throw new IllegalStateException("unsupported parameter type: " + parameter.getNestedParameterType());
+    }
+
+	MvcRequest resolveQueryComposer(QueryRequest ann, MethodParameter parameter, NativeWebRequest webRequest) {
+    	return cacheAttribute(webRequest, MvcRequest.class, ()->{
+        	var store = resolveStore(ann, parameter, null);
+    		var modifiableMap = new LinkedHashMap<>(webRequest.getParameterMap()); //modifiable map + preserve order
+    		if(!isEmpty(ann.ignore())) {
+    			for(var k : ann.ignore()) {
+    				if(modifiableMap.containsKey(k)) {
+    					log.debug("ignoring parameter '{}' as specified in @QueryRequest", k);
+    					modifiableMap.remove(k);
+    				}
+    			}
+    		}
+    		resolveParameterCompatibility(modifiableMap);
+    		modifiableMap.compute(VIEW_PARAM, (k,v)->{
+    			if(isEmpty(v) || ann.view().equals(v)) {
+    				return v;
+    			}
+    			throw new IllegalArgumentException("");
+    		});
+    		modifiableMap.computeIfAbsent(VIEW_PARAM, k-> DEFAULT_VIEWER);	
+    		modifiableMap.computeIfAbsent(SELECT_PARAM, k-> ann.fields());	
+        	return parseQuery(store, ann.dataset(), modifiableMap);
+    	});
+    }
+    
+    StoreResource resolveStore(QueryRequest ann, MethodParameter parameter, Class<? extends StoreResource> type) {
     	var store = ann.store() == StoreResource.class 
     			? getInstance().getDefaultStore() 
     			: getInstance().getStore(ann.store());
+    	if(nonNull(type) && !type.isInstance(store)) {
+    		throw new IllegalStateException("");
+    	}
         var rst = parameter.getParameterAnnotation(Restriction.class);
 		if(nonNull(rst)) {
 			store = restrict(store, rst.maxCols(), rst.maxRows(), rst.aggregate(), 
 					Set.of(rst.excludeResources()), Set.of(rst.excludeDialects()));
 		}
-		var modifiableMap = new LinkedHashMap<>(webRequest.getParameterMap()); //modifiable map + preserve order
-		if(!isEmpty(ann.ignore())) {
-			for(var k : ann.ignore()) {
-				if(modifiableMap.containsKey(k)) {
-					log.debug("ignoring parameter '{}' as specified in @QueryRequest", k);
-					modifiableMap.remove(k);
-				}
-			}
-		}
-		resolveParameterCompatibility(modifiableMap);
-		modifiableMap.computeIfAbsent(SELECT_PARAM, k-> ann.fields());	
-    	return parseQuery(store, ann.dataset(), modifiableMap);
+		return store;
     }
     
+    private static <T> T cacheAttribute(NativeWebRequest webRequest, Class<T> clazz, Supplier<? extends T> supplier) {
+		var att = webRequest.getAttribute(clazz.getName(), 0);
+		if(isNull(att)) {
+			att = supplier.get();
+			webRequest.setAttribute(clazz.getName(), att, 0);
+		}
+		return clazz.cast(att);
+	}
+
 	private static void resolveParameterCompatibility(Map<String, String[]> modifiableMap) {
-		Map.of(COLUMN_PARAM, SELECT_PARAM, FILTER_OPR, CRITERIA_OPR, VIEW_PARAM, CTE_PARAM).entrySet().forEach(e-> {
+		Map.of(COLUMN_PARAM, SELECT_PARAM, FILTER_OPR, CRITERIA_OPR).entrySet().forEach(e-> {
 			var args = modifiableMap.remove(e.getKey());
 			if(!isEmpty(args)) {
 				log.warn("'{}' parameter is deprecated, use {} instead", e.getKey(), e.getValue());
